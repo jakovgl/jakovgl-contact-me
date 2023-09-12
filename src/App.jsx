877000291ecbbbc79067218812993c6ed04d8541
Jakov Glavas <jakov@init.hr>
2023-09-12 14:53:12 +0200

    fix(App): Change contact email

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
         <p className={"last-name"}>Glavas</p>
         <p className={"job-title"}>Software Developer</p>
         <div className={"links"}>
-          <a href = "mailto: glavas.jakov22@gmail.com">email</a>
+          <a href = "mailto: me@jakovgl.com">email</a>
           <a href="https://github.com/jakovgl">github</a>
           <a href="https://hr.linkedin.com/in/jakov-glavas-849938193">linkedin</a>
         </div>
